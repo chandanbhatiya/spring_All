afe81d93c3567ce6eb7d47359eef63e385f89b62
chandan kumar <126801728+chandanbhatiya@users.noreply.github.com>
2023-07-29 06:57:46 +0530

    Add files via upload

---
+++ Spring_Inteceptor/src/main/java/com/spring/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.spring.controller;
+
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping("/profile")
+	public String profile(HttpSession session) {
+		return "profile";
+	}
+
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+
+		session.removeAttribute("loginUser");
+
+		return "redirect:/home";
+	}
+}
